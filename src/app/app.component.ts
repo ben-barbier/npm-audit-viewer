3e86799b0c2cc6929a8e6fbf696d5a3b6120f84d
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2021-02-22 15:41:53 +0100

    :rotating_light: run prettier globally

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
-import {Component, ElementRef, ViewChild} from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-
     @ViewChild('auditFile') auditFile: ElementRef<HTMLTextAreaElement> | undefined;
 
     parseAuditFile(): void {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,14 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        MatInputModule,
-        MatButtonModule,
-    ],
+    declarations: [AppComponent],
+    imports: [BrowserModule, BrowserAnimationsModule, MatInputModule, MatButtonModule],
     providers: [],
-    bootstrap: [AppComponent]
+    bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
