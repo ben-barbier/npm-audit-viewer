6b5f64b53cd1aff60383c009e9f1eb9fd85a942b
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2021-02-22 23:40:30 +0100

    :sparkles: add github link

---
+++ src/app/github-link/github-link.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-github-link',
+    templateUrl: './github-link.component.html',
+    styleUrls: ['./github-link.component.scss'],
+})
+export class GithubLinkComponent {}
