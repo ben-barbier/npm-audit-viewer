2aca3b58e3c5e39a229ba72f540cb2146cb3ee9c
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2021-02-23 00:19:34 +0100

    :lipstick: add package manager selector

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,9 @@ import { MatCardModule } from '@angular/material/card';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
 import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
@@ -21,6 +23,8 @@ import { GithubLinkComponent } from './github-link/github-link.component';
         MatDatepickerModule,
         MatNativeDateModule,
         MatCardModule,
+        MatRadioModule,
+        MatTooltipModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
