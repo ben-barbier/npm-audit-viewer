3c9ab90abc8830457a00accbd7cf8d305a78c21f
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2021-02-22 15:32:39 +0100

    :sparkles: add input and parse button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'npm-audit-viewer';
+
+    @ViewChild('auditFile') auditFile: ElementRef<HTMLTextAreaElement> | undefined;
+
+    parseAuditFile(): void {
+        const auditFileContent = this.auditFile?.nativeElement.value;
+
+        if (!auditFileContent) {
+            throw new Error('missing audit file content');
+        }
+
+        const auditFileObject = JSON.parse(auditFileContent);
+        debugger;
+    }
 }
